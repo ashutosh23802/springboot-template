b531791f04828cfcc6cbe32295a92f5172bd2ee0
ashutosh23802 <51711269+ashutosh23802@users.noreply.github.com>
2024-09-30 12:42:07 +0530

    Add files via upload

---
+++ src/main/java/org/tiaa/cache_support_services/tests/AutomatedTestsApplication.java
@@ -0,0 +1,13 @@
+package org.tiaa.cache_support_services.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tiaa/cache_support_services/tests/AutomatedTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tiaa.cache_support_services.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
